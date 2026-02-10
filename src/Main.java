5ea45d5526535453d0a4b39607f03068e2f80284
kidjanate <64061240+kidjanate@users.noreply.github.com>
2026-02-10 20:15:35 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import Base.Game;
 import Base.Renderer;
 import Game.TestScene;
-import Instances.Instance;
-import Instances.Sprite;
 
 public class Main {
     public static void main(String[] args) {
         var renderer = new Renderer();
         renderer.Run();
         renderer.LoadScene(new TestScene());
+        // test
     }
 }
